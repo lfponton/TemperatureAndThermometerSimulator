34a5b460b8424b6c862422d1c5855744ae9925db
Luis <lfponton@gmail.com>
2021-02-26 15:36:15 +0100

    temperature model classes

--- src/model/DateTime.java
+++ src/model/DateTime.java
@@ -1,5 +1,18 @@
 package model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateTime
 {
+  private Date date;
+
+  public DateTime() { date = Calendar.getInstance().getTime();
+  }
+
+  public String getTimeStamp() {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    return sdf.format(date);
+  }
 }
--- src/model/TemperatureList.java
+++ src/model/TemperatureList.java
@@ -1,5 +1,31 @@
 package model;
 
+import java.util.ArrayList;
+
 public class TemperatureList
 {
+  private ArrayList<Temperature> list;
+
+  public TemperatureList() {
+    this.list = new ArrayList<>();
+  }
+
+  public void addTemperature (Temperature temperature) {
+    list.add(temperature);
+  }
+
+  public Temperature getLastTemperature(String id) {
+    if (list.size() < 1) {
+      return null;
+    }
+    if (id == null) {
+      return list.get(list.size() - 1);
+    }
+    for (int i = list.size() - 1; i >= 0; i--) {
+      if (id.equals(list.get(i).getId())) {
+        return list.get(i);
+      }
+    }
+    return null;
+  }
 }
