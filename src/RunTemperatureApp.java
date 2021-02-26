9c4cc590f340f8b75e85e78a5bd6c01fdc581bfa
Luis <lfponton@gmail.com>
2021-02-26 18:22:19 +0100

    app and main classes

--- src/RunTemperatureApp.java
+++ src/RunTemperatureApp.java
@@ -1,3 +1,8 @@
+import javafx.application.Application;
+
 public class RunTemperatureApp
 {
+  public static void main(String[] args) throws InterruptedException {
+    Application.launch(TemperatureApp.class);
+  }
 }
--- src/TemperatureApp.java
+++ src/TemperatureApp.java
@@ -1,3 +1,23 @@
-public class TemperatureApp
+import core.ModelFactory;
+import core.ViewHandler;
+import core.ViewModelFactory;
+import external.Thermometer;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TemperatureApp extends Application
 {
+  @Override public void start(Stage stage) throws Exception
+  {
+    ModelFactory modelFactory = new ModelFactory();
+    ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
+    ViewHandler viewHandler = new ViewHandler(viewModelFactory);
+
+    viewHandler.start();
+
+    Thread t1 = new Thread(new Thermometer("t1", 15, 1,
+        modelFactory.getTemperatureModel()));
+
+    t1.start();
+  }
 }
