890de8ec7fa6068f6e063fe6ad41ddede1d215ce
Luis <lfponton@gmail.com>
2021-02-26 17:57:09 +0100

    thermometer class

--- src/view/temperature/TemperatureViewController.java
+++ src/view/temperature/TemperatureViewController.java
@@ -1,5 +1,31 @@
 package view.temperature;
 
+import core.ViewHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TemperatureViewController
 {
+  @FXML private Label outputLabel;
+  @FXML private TextField filterField;
+  @FXML private Label filterLabel;
+
+  private String thermometerId;
+  private ViewHandler viewHandler;
+  private TemperatureViewModel temperatureViewModel;
+
+  public void init(ViewHandler viewHandler,
+      TemperatureViewModel temperatureViewModel) {
+    this.viewHandler = viewHandler;
+    this.temperatureViewModel = temperatureViewModel;
+  }
+
+  @FXML private void onUpdateButton() {
+
+  }
+
+  @FXML private void onFilter() {
+
+  }
 }
--- src/view/temperature/TemperatureViewModel.java
+++ src/view/temperature/TemperatureViewModel.java
@@ -1,5 +1,21 @@
 package view.temperature;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.StringProperty;
+import mediator.TemperatureModel;
+
 public class TemperatureViewModel
 {
+  private DoubleProperty temperature;
+  private StringProperty id;
+  private TemperatureModel temperatureModel;
+
+  public TemperatureViewModel(TemperatureModel temperatureModel) {
+    this.temperatureModel = temperatureModel;
+  }
+
+  public void getLastTemp() {
+
+  }
+
 }
