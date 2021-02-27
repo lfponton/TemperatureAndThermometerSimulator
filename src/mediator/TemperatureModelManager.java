e73978e4da3c936e43555e20543705617a360d54
Luis <lfponton@gmail.com>
2021-02-27 11:58:16 +0100

    model manager

--- src/mediator/TemperatureModelManager.java
+++ src/mediator/TemperatureModelManager.java
@@ -1,28 +1,42 @@
 package mediator;
 
 import model.Temperature;
+import model.TemperatureList;
+import util.Subject;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
-public class TemperatureModelManager implements TemperatureModel
+public class TemperatureModelManager implements TemperatureModel, Subject
 {
+  private TemperatureList temperatureList;
+  private PropertyChangeSupport propertyChangeSupport;
+
+  public TemperatureModelManager() {
+    temperatureList = new TemperatureList();
+    propertyChangeSupport = new PropertyChangeSupport(this);
+  }
+
   @Override public void addTemperature(String id, double temperature)
   {
-
+    Temperature t = new Temperature(id, temperature);
+    Temperature old = getLastInsertedTemperature();
+    this.temperatureList.addTemperature(t);
+    propertyChangeSupport.firePropertyChange("Update", old, t);
   }
 
   @Override public Temperature getLastInsertedTemperature()
   {
-    return null;
+    return temperatureList.getLastTemperature(null);
   }
 
   @Override public Temperature getLastInsertedTemperature(String id)
   {
-    return null;
+    return temperatureList.getLastTemperature(id);
   }
 
   @Override public void addListener(PropertyChangeListener listener)
   {
-
+    propertyChangeSupport.addPropertyChangeListener(listener);
   }
 }
